d0fbe985bfb3f7327976e10e31233ab11b117d72
Noah Schutte <noahschutte@gmail.com>
2016-05-09 12:32:46 -0400

    refactor to mvc pattern

--- app.js
+++
@@ -1,4 +0,0 @@
-
-console.log( markdown.toHTML( "Hello *World*!" ) );
-
-
--- view.js
+++ view.js
@@ -1,22 +1,12 @@
-$(document).ready(function() {
+function View(){
 
-  $('#input').keyup(function(event){
-    var $target = $(event.target)
-    console.log($target.val())
-    view.drawOutput($target.val())
-  });
+}
 
-  function View(){
+View.prototype.drawOutput = function(arg){
+  var $target = $('#output');
+  var html = arg;
+  $target.html(transform(html));
+}
 
-  }
 
-  View.prototype.drawOutput = function(arg){
-    var $target = $('#output');
-    var html = arg;
-    $target.html(transform(html));
-  }
 
-  view = new View();
-  view.drawOutput();
-
-})
